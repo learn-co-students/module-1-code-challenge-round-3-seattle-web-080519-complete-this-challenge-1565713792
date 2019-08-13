d366805f009759a6ecc5bd0b55eeb17bc5e174f0
ecargiohc <therealgracechoi@gmail.com>
2019-08-13 10:31:19 -0700

    crrrushed it

--- tools/console.rb
+++ tools/console.rb
@@ -6,13 +6,21 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
+steinbeck = Author.new("John Steinbeck")
+wilde = Author.new("Oscar Wilde")
+vonnegut = Author.new("Kurt Vonnegut")
+hemingway = Author.new("Ernest Hemingway")
 
+newyorker = Magazine.new("New Yorker", "news")
+vanityfair = Magazine.new("Vanity Fair", "lifestyle")
+vogue = Magazine.new("Vogue", "fashion")
+travel = Magazine.new("Travel", "travel")
 
-
-
-
-
-
+a1 = Article.new(steinbeck, newyorker, "oh", "serious", 550)
+a2 = Article.new(wilde, vanityfair, "ooo", "chic", 1000)
+a3 = Article.new(vonnegut, vogue, "yess", "it", 300)
+a4 = Article.new(hemingway, travel, "wanderlust", "dreamy", 820)
+a5 = Article.new(wilde, vogue, "fab", "edgy", 240)
 
 
 ### DO NOT REMOVE THIS
