6deeab2556805cbe907d03088f42b5c2c86e15a9
Marisa <you@example.com>
2019-08-13 10:36:27 -0700

    Marisa

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,21 @@
 class Article
 
+attr_accessor :title, :content, :author, :magazine
+
+@@all = []
+
+def initialize(author, magazine, title, content)
+    @author = author
+    @magazine = magazine
+    @title = title
+    @content = content
+
+    @@all << self 
+end 
+
+def self.all
+    @@all 
+end 
+
 
 end
