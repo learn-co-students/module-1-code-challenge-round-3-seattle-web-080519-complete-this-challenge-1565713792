c3337924c6bbc2a0c5ea72ad9ebb72702ef520cf
AKhan-CIR <akhan@codeinreach.com>
2019-08-13 10:32:54 -0700

    Crushed it

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,50 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
+  @@all = []
 
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_article(magazine, title, content)
+    Article.new(self, magazine, title, content)
+  end
+
+  def articles #CHECKED ARRAY/ INSTANCES
+    Article.all.select{|art| art.author == self}
+  end
+
+  def my_magazines #HELPER METHOD
+    articles.map{|art| art.magazine}
+  end
   
+  def magazines #DONE UNIQ ARRAY
+    articles.map{|art| art.magazine}.uniq
+  end
+
+  def show_specialties
+    my_magazines.map{|mag| mag.category}.uniq
+  end
+
+  def self.most_active
+    author_list = Article.all.map{|art| art.author}
+    author_list.max_by{|author| author_list.count(author)}
+  end
+
+   
+  def self.most_verbose #DONE INSTANCE
+    longest = Article.all.max_by{|article| article.content.length}
+    longest.author
+    
   end
 
 
 end
+
+
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,11 +1,41 @@
-class Magazine
-  attr_accessor :name, :category
+class Magazine #CHECKED
+  attr_accessor :name, :category 
+  @@all = []
 
 
   def initialize(name, category)
     @name = name
     @category = category
+    @@all << self
 
+  end
+
+  def self.find_by_name(name) #DONE / TESTED "FARMING WEEKLY"
+    @@all.find{|magazine| magazine.name == name}
+  end
+
+  def my_articles #HELPER METHOD
+    Article.all.select{|art| art.magazine == self}
+  end
+
+  def article_titles #ARRAY / TITLES
+    my_articles.map{|art| art.title}
+  end
+
+  def contributors #ARRAY / INSTANCE
+    my_articles.map{|art| art.author}
+  end
+
+  def word_count #DONE / NUMBER
+    art_content = my_articles.map{|art| art.content}
+    joined_content = art_content.join(",")
+    split_content = joined_content.split(" ")
+    split_content.count
+  end
+
+
+  def self.all
+    @@all
   end
 
 
